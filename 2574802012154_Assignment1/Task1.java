746ae1b765c13dc00587c01cd70730afbbae0b56
Trannguyen <trannguyenxuan.27@gmail.com>
2026-01-29 19:32:25 +0700

    Add files via upload

---
+++ 2574802012154_Assignment1/Task1.java
@@ -0,0 +1,7 @@
+public class Task1 {
+    public static void main (String[] args) {
+        System.out.println(" name:Nguyen Xuan Tran");
+        System.out.println("age:16");
+        System.out.println("hobby: game, read book.listen music");
+    }
+}
